b809975d782ad3fec277b180c0257e2edd4eaf57
N1VIOLA <n1viola@student.bridgew.edu>
2021-09-14 10:34:34 -0400

    List make

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        var product = new ArrayList<String>();
+        var price = new ArrayList<Float>();
+        var numAvailable = new ArrayList<Integer>();
+        var fileName = "Product List";
+    }
+}
