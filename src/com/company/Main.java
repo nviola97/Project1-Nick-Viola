80f88d3563f054ffec66b105eda3619988902379
N1VIOLA <n1viola@student.bridgew.edu>
2021-09-21 10:52:16 -0400

    FINAL WORKING

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,26 +14,29 @@ public class Main {
         var fileName = "productList";
         var filePath = Paths.get(fileName);
         var allLines = Files.readAllLines(filePath);
-        for (var line: allLines){
-            var splitLine = line.split(",");
+        for (var line : allLines) {
+            var splitLine = line.split(", ");
             product.add(splitLine[0]);
             price.add(Float.parseFloat(splitLine[1]));
             numAvailable.add(Integer.parseInt(splitLine[2]));
-    }
-        //for (int i = 0; i< product.size(); i++){
-            //System.out.println( product.get(i)  +"     "+ price.get(i)+"      "  +  numAvailable.get(i));
-
+        }
         var productName = "";
         var reader = new Scanner(System.in);
-         while (!productName.equals("DONE")){
+        while (!productName.equals("DONE")) {
             System.out.println("Enter product name if done type DONE: ");
             productName = reader.nextLine();
-            for(int i = 0; i< product.size(); i++){
-                if (productName.equals(product)){
-
+            for (int i = 0; i < product.size(); i++) {
+                if (productName.equals(product.get(i))) {
+                    System.out.println(product.get(i) + " " + "$" + price.get(i) + " " + numAvailable.get(i) + " in stock");
+                    break;
                 }
+                if (i == product.size()-1){
+                    System.out.println("Item not found.");
+                    break;
+                }
+                if (productName == "DONE");
+                    break;
             }
-
         }
-        }
+    }
 }
